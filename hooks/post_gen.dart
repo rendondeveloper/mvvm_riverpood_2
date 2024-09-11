b8431c465a8fa5841bce86d9f9f447cfed6bfc37
luis rendon <luis.rendon@aplazo.mx>
2024-09-10 18:23:17 -0600

    change post

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,5 +1,23 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  // TODO: add post-generation logic.
+  final namePascalCase = context.vars['feature_name'].toString().pascalCase;
+  final filePath = 'lib/core/navigation/constants/route_paths.dart';
+  final file = File(filePath);
+
+  if (file.existsSync()) {
+    final content = file.readAsStringSync();
+
+    final updatedContent = content.replaceFirst(
+        '''//* TODO ADD NEW ROUTE - DON`T DELETE *//''',
+        '''const String page$namePascalCase = '$namePascalCase';\n
+//* TODO ADD NEW ROUTE - DON`T DELETE *//''');
+
+    file.writeAsStringSync(updatedContent);
+    context.logger.info('route_paths.dart has been updated.');
+  } else {
+    context.logger.err('route_paths.dart not found.');
+  }
 }
--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,23 +1,5 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  final namePascalCase = context.vars['feature_name'].toString().pascalCase;
-  final filePath = 'lib/core/navigation/constants/route_paths.dart';
-  final file = File(filePath);
-
-  if (file.existsSync()) {
-    final content = file.readAsStringSync();
-
-    final updatedContent = content.replaceFirst(
-        '''//* TODO ADD NEW ROUTE - DON`T DELETE *//''',
-        '''const String page$namePascalCase = '$namePascalCase';\n
-//* TODO ADD NEW ROUTE - DON`T DELETE *//''');
-
-    file.writeAsStringSync(updatedContent);
-    context.logger.info('route_paths.dart has been updated.');
-  } else {
-    context.logger.err('route_paths.dart not found.');
-  }
+  // TODO: add pre-generation logic.
 }
